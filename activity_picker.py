7d0e61becfc367bcd6a1c8f5c635bd50abb1affc
Bell Codo <bellcodo@gmail.com>
2016-08-26 16:58:06 -0700

    Create activity_picker.py

---
+++ activity_picker.py
@@ -0,0 +1,8 @@
+# import special libraries already built in python
+
+# list of options to select from
+possible_activities = []
+
+# choice of what we are going to do
+
+# display the results to the end user
