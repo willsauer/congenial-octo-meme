d5094af2053c4aa30416f3fd71bd79aaa5cd238f
Bell Codo <bellcodo@gmail.com>
2016-08-26 17:13:56 -0700

    Update activity_picker.py

--- activity_picker.py
+++ activity_picker.py
@@ -1,9 +1,11 @@
 # import special libraries already built in python
 
 # list of options to select from
-possible_activities = []
+possible_activities = ['a', 'b', 'c']
 
 # choice of what we are going to do
+the_activity = possible_activities[1]
 
 # display the results to the end user
 print "Possible activities are: " + str(possible_activities)
+print "What we are going to do: " + the_activity
