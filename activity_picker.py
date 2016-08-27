7f4ecf534dbb5f09c555886ffea9de171210d39c
Bell Codo <bellcodo@gmail.com>
2016-08-26 17:45:21 -0700

    Update activity_picker.py

--- activity_picker.py
+++ activity_picker.py
@@ -5,9 +5,9 @@ import random, urllib2
 the_url = 'https://raw.githubusercontent.com/bellcodo/congenial-octo-meme/master/activities.lst'
 list_raw_text = urllib2.urlopen(the_url).read()
 
-print "DEBUG: " + str(list_raw_text.split())
+# print "DEBUG: " + str(list_raw_text.split())
 
-possible_activities = ['a', 'b', 'c']
+possible_activities = list_raw_text.split()
 
 # choice of what we are going to do
 the_activity = random.choice(possible_activities)
